dfc6da679eee1c390ce1371e186a7452b21b21c0
piol14 <eof812@gmail.com>
2023-11-14 00:07:07 +0100

    dfsfsrf<

--- src/main/java/net/ausiasmarch/foxforumserver/entity/ClienteEntity.java
+++ src/main/java/net/ausiasmarch/foxforumserver/entity/ClienteEntity.java
@@ -2,6 +2,8 @@ package net.ausiasmarch.foxforumserver.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,6 +32,7 @@ public class ClienteEntity {
     }
 
     @OneToMany(mappedBy = "cliente")
+    @JsonIgnore
     private List<AlquilerEntity> alquileres;
 
     public ClienteEntity() {
